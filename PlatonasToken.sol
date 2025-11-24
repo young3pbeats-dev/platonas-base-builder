40f78d8dd500957959b278563015b9896a25de86
young3pbeats-dev <young3pbeats@gmail.com>
2025-11-24 15:53:10 +0100

    Create PlatonasToken.sol
    ## Platonas Token (ERC20)
    
    - **Network:** Base Mainnet  
    - **Symbol:** PLATONAS  
    - **Total Supply:** 1,000,000 PLATONAS  
    - **Contract:** https://basescan.org/address/0xBD8BbB91213AfaFC8342acbb383e99C094dC1C99

---
+++ PlatonasToken.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract PlatonasToken {
+    string public name = "Platonas Token";
+    string public symbol = "PLATONAS";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        uint256 initialSupply = 1_000_000 * (10 ** uint256(decimals));
+        totalSupply = initialSupply;
+        balanceOf[msg.sender] = initialSupply;
+        emit Transfer(address(0), msg.sender, initialSupply);
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Not enough balance");
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(balanceOf[from] >= value, "Not enough balance");
+        require(allowance[from][msg.sender] >= value, "Allowance too low");
+
+        allowance[from][msg.sender] -= value;
+        _transfer(from, to, value);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 value) internal {
+        require(to != address(0), "Zero address");
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        emit Transfer(from, to, value);
+    }
+}
